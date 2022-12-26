584885be0eecaae709f7c9767b58908e5cf6613b
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2022-12-26 10:20:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Editor from './component/Editor'; 
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Editor />
+       
+    </div>
+  );
+}
+
+
+export default App;
